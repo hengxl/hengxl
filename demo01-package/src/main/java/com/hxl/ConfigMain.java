36324b1d81116ffe52f3b691349c303f7b6e9ae2
hengxiaoliang <1718615328@qq.com>
2025-09-14 18:07:40 +0800

    hengxl:1

--- demo01-package/src/main/java/com/hxl/ConfigMain.java
+++ demo01-package/src/main/java/com/hxl/ConfigMain.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConfigMain {
     public static void main(String[] args) {
         SpringApplication.run(ConfigMain.class, args);
+        System.out.println("修改喽喽");
     }
 }
